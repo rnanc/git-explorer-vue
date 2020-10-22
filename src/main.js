46773e88fdea18779fe24cc08d68604c0174a94e
renan <renan@localhost.localdomain>
2020-10-22 17:25:00 -0300

    feat: icons added

--- src/main.js
+++ src/main.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
-// import Vuetify from 'vuetify/lib'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 import Home from './components/Home'
 import Description from './components/Description'
 
+library.add(faCoffee)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 Vue.use(VueRouter)
-// Vue.use(Vuetify)
 
 const routes =[
   {name: "home", path: '/', component: Home},
